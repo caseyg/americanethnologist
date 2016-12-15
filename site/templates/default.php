bd7b77ee938f8fddf7efef6630a8387ea31dd417
Auto Git <autogit@localhost>
2016-12-14 21:32:47 -0500

    Show text on default template

--- site/templates/default.php
+++ site/templates/default.php
@@ -14,6 +14,7 @@
     </div>
     <div class="col-sm-8 pull-sm-3">
       <?php $page->modules() ?>
+      <?php echo $page->text()->kt() ?>
     </div>
   </div>
 
@@ -22,6 +23,7 @@
   <div class="row">
     <div class="col-xs-12">
       <?php $page->modules() ?>
+      <?php echo $page->text()->kt() ?>
     </div>
   </div>
 
