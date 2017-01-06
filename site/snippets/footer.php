f946d5edbc763f786e3ce67879a6b3661f0369df
Casey Gollan <casey@bullshit.systems>
2017-01-05 20:45:23 -0500

    Enable panel bar

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -8,5 +8,6 @@
   <script data-no-instant src="https://use.fontawesome.com/5450ef4433.js"></script>
   <script data-no-instant src="/assets/js/instantclick.min.js"></script>
   <script data-no-instant>InstantClick.init('mousedown');</script>
+  <?php echo panelBar::show() ?>
 </body>
 </html>
