d04dca59fbe862af13b52a0ca4412058ecc7d324
Casey Gollan <casey@bullshit.systems>
2017-01-19 15:54:43 -0500

    Cleaning up homepage and footer

--- site/patterns/navigation/footer/footer.html.php
+++ site/patterns/navigation/footer/footer.html.php
@@ -1,12 +1,99 @@
-<div class="container-fluid bg-green">
+<div class="container-fluid bg-green" style="background-image: url('/assets/images/aes-logo-footer.png');background-position:top left;background-repeat:no-repeat;">
   <div class="container">
-    <?php snippet("logo") ?>
+<!--    <?php snippet("logo") ?>-->
     <footer class="footer pt-3 py-3" role="contentinfo">
+      <div class="row">
+        <div class="col-xs-3">
+          <h6>American Ethnological Society</h6>
+          <ul class="list-unstyled">
+            <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</a></li>
+            <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i> Search</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-3">
+          <h6>Society</h6>
+          <ul class="list-unstyled">
+            <li><a href="#">About</a>
+              <ul>
+                <li><a href="#">Board</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Publications</a></li>
+                <li><a href="#">Editorial Interns</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Join</a></li>
+            <li><a href="#">Awards</a>
+              <ul>
+                <li><a href="">Sharon Stephens Prize</a></li>
+                <li><a href="">Senior Book Prize</a></li>
+                <li><a href="">Elsie Clews Parsons Prize</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xs-3">
+          <h6>Journal</h6>
+          <ul class="list-unstyled">
+            <li><a href="#">Read</a>
+              <ul>
+                <li><a href="">Current Issue</a></li>
+                <li><a href="">Browse</a></li>
+                <li><a href="">Book Reviews</a></li>
+                <li><a href="">Access &amp; Subscriptions</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Submit</a>
+              <ul>
+                <li><a href="http://americanethnologist.dev/submit/kinds-of-submissions">KInds of Submissions</a></li>
+                <li><a href="http://americanethnologist.dev/submit/writing-style">Writing</a></li>
+                <li><a href="http://americanethnologist.dev/submit/stylesheet">Stylesheet</a></li>
+                <li><a href="http://americanethnologist.dev/submit/tables-and-illustrations">Tables and Illustrations</a></li>
+                <li><a href="http://americanethnologist.dev/submit/submission-guidelines">Submission Guidelines</a></li>
+                <li><a href="http://americanethnologist.dev/submit/books-for-reviews">Books for Reviews</a></li>
+                <li><a href="http://americanethnologist.dev/submit/partnership-with-sapiens">Partnership with SAPIENS</a></li>
+                <li><a href="http://americanethnologist.dev/submit/aaa-author-agreement-form">AAA Author Agreement Form</a></li>
+              </ul>
+            </li>
+            <li><a href="">Masthead</a>
+              <ul>
+                <li><a href="">Editorial Staff</a></li>
+                <li><a href="">Editorial Board</a></li>
+                <li><a href="">Past Editors</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xs-3">
+          <h6>Conversations</h6>
+          <ul class="list-unstyled">
+            <li><a href="#">Features</a>
+              <ul>
+                <li><a href="#">Series</a></li>
+                <li><a href="#">Interviews</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Meetings</a>
+              <ul>
+                <li><a href="#">Spring Conference</a></li>
+                <li><a href="#">AAA Sessions</a></li>
+              </ul>
+            </li>
+            <li>Social Media
+            <ul>
+              <li><a href="http://twitter.com/amethno"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+              <li><a href="http://facebook.com/americanethnologist/"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a></li>
+              <li><a href="https://www.youtube.com/channel/UCoyoY7UMQBtcqrDnekyC6HA"><i class="fa fa-youtube" aria-hidden="true"></i> YouTube</a></li>
+            </ul>
+            </li>
+          </ul>
+
+        </div>
+      </div>
       <p class="footer-copyright"><?php
-        // Parse Kirbytext to support dynamic year,
-        // but remove all HTML like paragraph tags:
         echo html::decode($site->copyright()->kirbytext())
-      ?></p>
+      ?> American Ethnological Society</p>
     </footer>
   </div>
 </div>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,57 +1,17 @@
-<!doctype html>
-<html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
-<head>
+<?php snippet('header') ?>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+</div>
 
-  <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
-  <meta name="description" content="<?= $site->description()->html() ?>">
+<div class="container-fluid bg-blockquote">
+  <div class="container pt-3 pb-3">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1 class="bentonsanscondensedregular green">Welcome to the <strong class="bentonsanscondensedbold">American Ethnological Society</strong>. We study all the different cultural ways there are to be human. And we've been leading the way in doing so <a href="/history">since 1842</a>.</h1>
 
-  <link href="//cloud.typenetwork.com/projects/578/fontface.css/" rel="stylesheet" type="text/css">
-  <script src="https://use.typekit.net/zsj4lxh.js"></script>
-  <script>try{Typekit.load({ async: true });}catch(e){}</script>
-  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">-->
-  <?= css('assets/css/bootstrap.min.css') ?>
-  <?= css('assets/css/index.min.css') ?>
-
-</head>
-<body>
-  <div class="container">
-    <header class="row pt-2 mb-2">
-      <div class="col-xs-12 col-sm-5">
-        <h1><a href="/"><span class="bentonsanscondensedblack">(&aelig;s)</span> American<br> Ethnological Society</a></h1>
+          <?php $page->modules() ?>
+        </div>
       </div>
-      <div class="col-xs-4 col-sm-2">
-        <ul class="list-unstyled">
-          <li><strong><?php echo $site->nav_col1() ?></strong></li>
-          <?php foreach ($site->nav_col1_links()->toStructure() as $p): ?>
-            <li><a <?php e($site->find($p)->isOpen(), ' class="active"') ?> href="<?php echo $site->find($p)->url() ?>"><?php echo $site->find($p)->title() ?></a></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <div class="col-xs-4 col-sm-2">
-        <ul class="list-unstyled">
-          <li><strong><?php echo $site->nav_col2() ?></strong></li>
-          <?php foreach ($site->nav_col2_links()->toStructure() as $p): ?>
-            <li><a <?php e($site->find($p)->isOpen(), ' class="active"') ?> href="<?php echo $site->find($p)->url() ?>"><?php echo $site->find($p)->title() ?></a></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <div class="col-xs-4 col-sm-2">
-        <ul class="list-unstyled">
-          <li><strong><?php echo $site->nav_col3() ?></strong></li>
-          <?php foreach ($site->nav_col3_links()->toStructure() as $p): ?>
-            <li><a <?php e($site->find($p)->isOpen(), ' class="active"') ?> href="<?php echo $site->find($p)->url() ?>"><?php echo $site->find($p)->title() ?></a></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-1 text-xs-center text-sm-right">
-        <ul class="list-unstyled">
-          <li class="col-xs-4 col-xs-12"><a href="/search"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-    </a></li>
-        </ul>
-      </div>
-    </header>
+  </div>
+</div>
 
 <?php snippet('footer') ?>
