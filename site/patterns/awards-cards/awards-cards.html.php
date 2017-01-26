3c48e8a662095fe1e26fdee125f2ce9a13a89f5a
Casey Gollan <casey@bullshit.systems>
2017-01-26 14:26:23 -0500

    Add list style to awards cards, #14

--- site/patterns/awards-cards/awards-cards.html.php
+++ site/patterns/awards-cards/awards-cards.html.php
@@ -8,20 +8,38 @@
     </div>
     <div class="col-sm-8">
       <?php if ($p->children()->count() > 0): ?>
-        <div class="row">
-          <?php foreach ($p->children()->visible()->limit(3) as $p): ?>
-            <a class="sans col-sm-4" href="<?php echo $p->url() ?>">
-              <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
-              <?php if ($p->images()->count() > 0): ?>
-                <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
-              <?php else: ?>
-                <img src="http://placehold.it/223x335" class="img-fluid mb-1" alt="">
-              <?php endif; ?>
-              <h4><?php echo $p->awardee_title() ?></h4>
-              <h6>by <?php echo $p->awardee_name() ?></h6>
-            </a>
-          <?php endforeach; ?>
-        </div>
+        <?php if ($p->display_as_list() == '1'): ?>
+          <div class="row">
+            <hr>
+            <ul class="list-unstyled">
+              <?php foreach ($p->children()->visible()->limit(3) as $p): ?>
+                <li>
+                  <a class="sans" href="<?php echo $p->url() ?>">
+                    <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
+                    <h4><?php echo $p->awardee_title() ?></h4>
+                    <h6>by <?php echo $p->awardee_name() ?></h6>
+                  </a>
+                </li>
+                <hr>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        <?php else: ?>
+          <div class="row">
+            <?php foreach ($p->children()->visible()->limit(3) as $p): ?>
+              <a class="sans col-sm-4" href="<?php echo $p->url() ?>">
+                <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
+                <?php if ($p->images()->count() > 0): ?>
+                  <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
+                <?php else: ?>
+                  <img src="http://placehold.it/223x335" class="img-fluid mb-1" alt="">
+                <?php endif; ?>
+                <h4><?php echo $p->awardee_title() ?></h4>
+                <h6>by <?php echo $p->awardee_name() ?></h6>
+              </a>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
       <?php endif; ?>
     </div>
   </div>
