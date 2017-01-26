3dae1d5b9b8cd9aba82c5a112b95b486f12b6e65
Casey Gollan <casey@bullshit.systems>
2017-01-26 16:02:17 -0500

    Conditional images on features #55

--- site/templates/features.php
+++ site/templates/features.php
@@ -14,18 +14,20 @@
   } elseif ($page->depth() == 2) {
     $pages = $page->children();
   } ?>
-  <?php foreach ($pages->sortby('date') as $i): ?>
+  <?php foreach ($pages->visible()->sortby('date') as $i): ?>
     <li class="mb-1">
       <a href="<?php echo $i->url() ?>">
         <div class="media">
-          <div class="media-left">
-            <img class="media-object" src="http://placehold.it/100x100" alt="Generic placeholder image">
-          </div>
           <div class="media-body">
             <h6 class="bentonsansreregular"><?php echo $i->parent()->title() ?></h6>
             <h5 class="media-heading bentonsansrebold" style="line-height:1.35;"><?php echo $i->title() ?></h6>
             <h6 class="bentonsansreregular"><?php echo $i->date("M d, Y") ?></h6>
           </div>
+          <?php if ($i->hasImages()): ?>
+            <div class="media-right">
+              <img class="media-object" src="<?php echo $i->images()->first()->resize(100,100)->url() ?>" alt="Generic placeholder image">
+            </div>
+          <?php endif; ?>
         </div>
       </a>
     </li>
