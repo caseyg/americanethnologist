008a12267921dfee520714f8090438b61d13437e
Casey Gollan <casey@bullshit.systems>
2017-01-26 16:56:55 -0500

    Add modules folder to meetings

--- site/templates/meeting.php
+++ site/templates/meeting.php
@@ -1,14 +1,17 @@
 <?php snippet('header') ?>
 
+<div class="jumbotron">
+  <h1><?php echo $page->theme() ?></h1>
+  <p class="sans">
+    <?php echo $page->dates() ?><br>
+    <?php echo $page->location() ?>
+  </p>
+</div>
 <div class="row">
   <div class="col-sm-3 push-sm-9">
     <?php pattern('navigation/sidebar', array('pages' => $page->parent()->children()->visible())) ?>
   </div>
   <div class="col-sm-8 pull-sm-3">
-    <h1><?php echo $page->theme() ?></h1>
-    <p class="sans"><?php echo $page->dates() ?></p>
-    <p class="sans"><?php echo $page->location() ?></p>
-
     <?php $page->modules() ?>
     <?php echo $page->text()->kt() ?>
   </div>
