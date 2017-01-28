94c21235cebc6d79bb19ee4435575d28be2e93c2
Kirby Auto Git <autogit@localhost>
2017-01-28 17:45:43 -0500

    Enable instant click

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,8 +6,8 @@
   <script data-no-instant src="/assets/js/tether.min.js"></script>
   <script data-no-instant src="/assets/js/bootstrap.min.js"></script>
   <script data-no-instant src="https://use.fontawesome.com/5450ef4433.js"></script>
-<!--  <script data-no-instant src="/assets/js/instantclick.min.js"></script>
-  <script data-no-instant>InstantClick.init('mousedown');</script>-->
+  <script data-no-instant src="/assets/js/instantclick.min.js"></script>
+  <script data-no-instant>InstantClick.init('mousedown');</script>
   <?php echo panelBar::show() ?>
 </body>
 </html>
