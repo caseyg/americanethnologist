cac0121318b13523f41013f646fe817bed53e1b5
aesonlin <aesonlin@a2ss7.a2hosting.com>
2017-02-22 07:42:28 -0500

    content updates

--- site/config/config.php
+++ site/config/config.php
@@ -31,6 +31,8 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 c::set('debug',true);
 
+c::set('cache',true);
+
 c::set('patterns.title', 'AES Styleguide');
 c::set('patterns.path', 'styleguide');
 c::set('patterns.preview.css', ['https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css', '//cloud.typenetwork.com/projects/578/fontface.css/', 'assets/css/index.min.css', 'assets/css/styleguide.css']);
