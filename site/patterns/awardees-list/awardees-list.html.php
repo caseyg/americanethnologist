b9dff10693421f6c543ca29970631bba3c4877fa
Casey Gollan <casey@bullshit.systems>
2017-03-10 16:32:26 -0500

    adjust list styling, fixes #15

--- site/patterns/awardees-list/awardees-list.html.php
+++ site/patterns/awardees-list/awardees-list.html.php
@@ -1,25 +1,45 @@
-<?php $i = 0;
-foreach ($page->children()->visible()->flip() as $p): $i++; ?>
-  <?php if ($p->award_year()->isNotEmpty() && $p->award_type()->isNotEmpty() && $p->awardee_title()->isNotEmpty() && $p->awardee_name()->isNotEmpty()): ?>
-    <a class="sans col-sm-3" href="<?php echo $p->url() ?>">
-      <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
-      <?php if ($p->images()->count() > 0): ?>
-        <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
-      <?php else: ?>
-        <img src="http://placehold.it/255x383" class="img-fluid mb-1" alt="">
-      <?php endif; ?>
-      <h4><?php echo $p->awardee_title() ?></h4>
-      <h6>by <?php echo $p->awardee_name() ?></h6>
-    </a>
-  <?php else: ?>
-    <a class="sans col-sm-3" href="<?php echo $p->url() ?>">
-      <?php if ($p->images()->count() > 0): ?>
-        <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
-      <?php else: ?>
-        <img src="http://placehold.it/255x383" class="img-fluid mb-1" alt="">
-      <?php endif; ?>
-      <h6><?php echo $p->title() ?></h6>
-    </a>
-  <?php endif; ?>
-  <?php e($i % 4 == 0, '<div class="clearfix"></div>') ?>
-<?php endforeach; ?>
+<?php if($page->display_as_list() == '1'): ?>
+  <hr>
+    <ul class="list-unstyled">
+      <?php foreach($page->children()->visible()->flip() as $p): ?>
+        <li>
+          <a class="sans row" href="<?php echo $p->url() ?>">
+            <div class="col-xs-12 col-sm-3 text-sm-right">
+              <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
+            </div>
+            <div class="col-xs-12 col-sm-9">
+              <h4><?php echo $p->awardee_title() ?></h4>
+              <h6>by <?php echo $p->awardee_name() ?></h6>
+            </div>
+          </a>
+        </li>
+        <hr>
+      <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+  <?php $i = 0;
+  foreach ($page->children()->visible()->flip() as $p): $i++; ?>
+    <?php if ($p->award_year()->isNotEmpty() && $p->award_type()->isNotEmpty() && $p->awardee_title()->isNotEmpty() && $p->awardee_name()->isNotEmpty()): ?>
+      <a class="sans col-xs-6 col-md-3 mb-3" href="<?php echo $p->url() ?>">
+        <?php if ($p->images()->count() > 0): ?>
+          <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
+        <?php else: ?>
+          <img src="http://placehold.it/255x383" class="img-fluid mb-1" alt="">
+        <?php endif; ?>
+        <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
+        <h4><?php echo $p->awardee_title() ?></h4>
+        <h6>by <?php echo $p->awardee_name() ?></h6>
+      </a>
+    <?php else: ?>
+      <a class="sans col-xs-6 col-md-3" href="<?php echo $p->url() ?>">
+        <?php if ($p->images()->count() > 0): ?>
+          <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
+        <?php else: ?>
+          <img src="http://placehold.it/255x383" class="img-fluid mb-1" alt="">
+        <?php endif; ?>
+        <h6><?php echo $p->title() ?></h6>
+      </a>
+    <?php endif; ?>
+    <?php e($i % 4 == 0, '<div class="clearfix"></div>') ?>
+  <?php endforeach; ?>
+<?php endif; ?>
--- site/patterns/awards-cards/awards-cards.html.php
+++ site/patterns/awards-cards/awards-cards.html.php
@@ -1,17 +1,17 @@
 <?php foreach ($page->children()->visible() as $p): ?>
   <div class="row mt-3">
-    <div class="col-sm-4">
+    <div class="col-xs-12 col-sm-4">
       <h3><a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a></h3>
       <p><?php echo $p->intro()->kt() ?></p>
       <p><?php echo $p->deadline() ?></p>
       <p><a class="btn btn-secondary bg-background" href="<?php echo $p->url() ?>#more">Read more</a> <?php if ($p->children()->count() > 0): ?><a class="btn btn-secondary bg-background" href="<?php echo $p->url() ?>#awardees">Browse awardees</a><?php endif; ?></p>
     </div>
-    <div class="col-sm-8">
+    <div class="col-xs-12 col-sm-8">
       <?php if ($p->children()->count() > 0): ?>
         <?php if ($p->display_as_list() == '1'): ?>
           <div class="row">
             <hr>
-            <ul class="list-unstyled">
+            <ul class="list-unstyled col-xs-12">
               <?php foreach ($p->children()->visible()->flip()->limit(3) as $p): ?>
                 <li>
                   <a class="sans" href="<?php echo $p->url() ?>">
@@ -27,13 +27,13 @@
         <?php else: ?>
           <div class="row">
             <?php foreach ($p->children()->visible()->flip()->limit(3) as $p): ?>
-              <a class="sans col-sm-4" href="<?php echo $p->url() ?>">
-                <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
+              <a class="sans col-xs-6 col-md-4 mb-2" href="<?php echo $p->url() ?>">
                 <?php if ($p->images()->count() > 0): ?>
                   <img src="<?php echo $p->images()->first()->url() ?>" class="img-fluid mb-1" alt="">
                 <?php else: ?>
                   <img src="http://placehold.it/223x335" class="img-fluid mb-1" alt="">
                 <?php endif; ?>
+                <h6><?php echo $p->award_year() ?> <?php echo $p->award_type() ?></h6>
                 <h4><?php echo $p->awardee_title() ?></h4>
                 <h6>by <?php echo $p->awardee_name() ?></h6>
               </a>
