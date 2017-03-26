e4c52ec8651ae7c248e4e6b5870fd1029878cb6c
Casey Gollan <casey@bullshit.systems>
2017-03-26 15:40:12 -0400

    fix broken cover bug and hide invisible issues

--- site/templates/browse-issue.php
+++ site/templates/browse-issue.php
@@ -9,12 +9,12 @@
     <?php pattern('navigation/sidebar', array('pages' => $pages->find('read/browse')->children()->visible())) ?>
   </div>
   <div class="col-sm-8 pull-sm-3">
-    <?php foreach($site->index()->filterBy('template', 'journal')->filterBy('volume', '!=', '')->groupBy('volume') as $volume => $items): ?>
+    <?php foreach($site->index()->visible()->filterBy('template', 'journal')->filterBy('volume', '!=', '')->groupBy('volume') as $volume => $items): ?>
       <div class="row">
         <h2 class="h6 mb-1 col-xs-12">Volume <?php echo $volume ?></h2>
         <?php foreach ($items as $issue): ?>
           <a href="<?php echo $issue->url() ?>" class="col-xs-6 col-sm-3 mb-2">
-            <img src="<?php echo $issue->images()->first()->url() ?>" class="img-fluid" style="box-shadow: 0 5px 10px rgba(0,0,0,.25);">
+            <img src="<?php if ($issue->hasImages()): ?><?php echo $issue->images()->first()->url() ?><?php else: ?>http://placehold.it/160x209<?php endif; ?>" class="img-fluid" style="box-shadow: 0 5px 10px rgba(0,0,0,.25);">
             <h2 class="h5 mt-1 mb-0 pb-0">Issue <?php echo $issue->issue() ?></h2>
             <p class="sans h5"><?php echo date('F Y', $issue->date()) ?></p></a>
         <?php endforeach; ?>
