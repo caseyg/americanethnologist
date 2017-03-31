3f96effd57b6489f8b18584bf45cbad825a819b9
Casey Gollan <casey@bullshit.systems>
2017-03-31 11:54:05 -0400

    Fix #54

--- site/patterns/text/text.html.php
+++ site/patterns/text/text.html.php
@@ -1,3 +1,3 @@
-	<div class="text<?php if($module->lead() == "true"): ?> lead<?php if($module->page()->template() == "feature"): echo ""; endif; ?><?php endif ?> <?php if($module->page()->template() == "feature" && $module->lead() == "false"): echo " col-sm-6 push-sm-3"; endif; ?>" id="<?php echo $module->slug() ?>">
+	<div class="text<?php if($module->lead() == "true"): ?> lead<?php if($module->page()->template() == "feature"): echo ""; endif; ?><?php endif ?> <?php if($module->page()->template() == "feature" && $module->lead() == "false"): echo " col-sm-6 push-sm-3"; endif; ?>" id="m-<?php echo $module->slug() ?>">
 		<?= $module->text()->kt() ?>
 	</div>
