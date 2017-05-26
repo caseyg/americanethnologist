973fab24a3ed50def38a5e37d90ab9cb8f25fde7
Casey Gollan <casey@bullshit.systems>
2017-05-26 12:13:46 -0400

    reorganize features section

--- site/templates/features.php
+++ site/templates/features.php
@@ -2,19 +2,46 @@
 
 <?php if ($page->depth() > 1): ?>
   <?php pattern('navigation/horizontal', array('pages' => $page->siblings()->visible())) ?>
-<?php else: ?>
-  <?php pattern('navigation/horizontal', array('pages' => $page->children()->visible())) ?>
+  <hr>
 <?php endif; ?>
 
-<hr>
+<div class="row">
+<?php if ($page->depth() == 1): ?>
+  <?php foreach ($page->children()->visible() as $i): ?>
+    <div class="col-md-6 mb-1">
+      <h2 class="h4 text-xs-center"><a href="<?php echo $i->url() ?>"><?php echo $i->title() ?></a></h2>
+      <ul class="list-unstyled">
+        <?php $n = 0; ?>
+        <?php foreach ($i->children()->visible()->limit(3) as $i): $n++; ?>
+          <li class="mb-1">
+            <a href="<?php echo $i->url() ?>">
+              <div class="media">
+                <?php if ($n == 1): ?>
+                  <?php if ($i->hasImages()): ?>
+                    <img class="media-object img-fluid" src="<?php echo $i->images()->first()->resize(400,300)->url() ?>">
+                  <?php endif; ?>
+                <?php endif; ?>
+                <div class="media-body">
+                  <h5 class="media-heading bentonsansrebold" style="line-height:1.35;"><?php echo $i->title() ?></h6>
+                  <h6 class="bentonsansreregular"><?php echo $i->date("M d, Y") ?></h6>
+                </div>
+              </div>
+            </a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  <?php endforeach; ?>
+<?php endif; ?>
+</div>
 
 <ul class="list-unstyled">
-  <?php if ($page->depth() == 1) {
-    $pages = $page->grandchildren();
-  } elseif ($page->depth() == 2) {
+  <?php if ($page->depth() == 2) {
     $pages = $page->children();
   } ?>
-  <?php foreach ($pages->visible()->sortBy('date', 'desc') as $i): ?>
+  <?php
+    if ($page->depth() == 2):
+    foreach ($pages->visible()->sortBy('date', 'desc') as $i): ?>
     <li class="mb-1">
       <a href="<?php echo $i->url() ?>">
         <div class="media">
@@ -32,7 +59,9 @@
       </a>
     </li>
     <hr>
-  <?php endforeach; ?>
+  <?php endforeach;
+  endif;?>
+
 </ul>
 
 <?php $page->modules() ?>
