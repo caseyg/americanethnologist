bdcf29245f62e54c8acf1f1e15d836ec4c97d21a
Casey Gollan <casey@bullshit.systems>
2017-10-07 14:52:07 -0400

    add test for collections

---
+++ site/templates/collection.php
@@ -0,0 +1,33 @@
+<?php snippet('header') ?>
+
+<?php pattern('navigation/horizontal', array('pages' => $page->parent()->siblings()->visible())) ?>
+
+<hr>
+
+  <main class="main" role="main">
+    <?php if ($page->coverimage()->isNotEmpty()): ?>
+      <div class="row text-xs-center">
+        <img class="my-2 img-fluid" style="box-shadow: 0 5px 10px rgba(0,0,0,.25);" src="<?php echo $page->coverimage()->toFile()->resize(350,0)->url() ?>">
+      </div>
+    <?php endif; ?>
+    <div class="row">
+      <h1 class="text-xs-center"><?= $page->title()->html() ?></h1>
+    </div>
+    <div class="row">
+      <div class="lead col-sm-8 push-sm-2">
+        <?= $page->text()->kirbytext() ?>
+      </div>
+    </div>
+    <div class="row">
+      <?php foreach ($page->children()->visible() as $a): ?>
+        <?php $a = page($a); ?>
+        <div class="row">
+          <a href="<?php echo $a->url() ?>" class="mt-3 d-block text-xs-center">
+            <h3 class="mb-0"><?php echo $a->title() ?></h3>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </main>
+
+<?php snippet('footer') ?>
