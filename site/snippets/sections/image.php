568c3183cb7244507b27dc8fb2fd6315d95f8fc5
Casey Gollan <casey@bullshit.systems>
2017-11-02 11:51:30 -0400

    fixes #104

--- site/snippets/sections/image.php
+++ site/snippets/sections/image.php
@@ -1,5 +1,7 @@
 <?php $image = $data->image()->toFile() ?>
-<figure class="image <?= $data->class(); ?>">
-  <img src="<?= $image->url(); ?>" width="<?= $image->width(); ?>" height="<?= $image->height(); ?>" alt="<?= $image->description(); ?><?php if ($image->credit()->isNotEmpty()): ?> (<?= $image->credit() ?>)<?php endif; ?>">
-  <p><?= $image->description(); ?> <?php if ($image->credit()->isNotEmpty()): ?><small>(<?= $image->credit() ?>)</small><?php endif; ?></p>
-</figure>
+  <?php if($image): ?>
+	<figure class="image <?= $data->class(); ?>">
+	  <img src="<?= $image->url(); ?>" width="<?= $image->width(); ?>" height="<?= $image->height(); ?>" alt="<?= $image->description(); ?><?php if ($image->credit()->isNotEmpty()): ?> (<?= $image->credit() ?>)<?php endif; ?>">
+	  <p><?= $image->description(); ?> <?php if ($image->credit()->isNotEmpty()): ?><small>(<?= $image->credit() ?>)</small><?php endif; ?></p>
+	</figure>
+  <?php endif; ?>
