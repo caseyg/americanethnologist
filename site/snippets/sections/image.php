e22f48f3d91f96433aa91e2f392e0fa96d189341
Casey Gollan <casey@bullshit.systems>
2017-11-08 06:00:07 -0500

    show image captions & center, fixes #115

--- site/snippets/sections/image.php
+++ site/snippets/sections/image.php
@@ -2,6 +2,6 @@
   <?php if($image): ?>
 	<figure class="image <?= $data->class(); ?>">
 	  <img src="<?= $image->url(); ?>" width="<?= $image->width(); ?>" height="<?= $image->height(); ?>" alt="<?= $image->description(); ?><?php if ($image->credit()->isNotEmpty()): ?> (<?= $image->credit() ?>)<?php endif; ?>">
-	  <p><?= $image->description(); ?> <?php if ($image->credit()->isNotEmpty()): ?><small>(<?= $image->credit() ?>)</small><?php endif; ?></p>
+	  <figcaption class="bodyText col-lg-8 offset-lg-2"><?= $image->caption(); ?> <?php if ($image->credit()->isNotEmpty()): ?><small>(<?= $image->credit() ?>)</small><?php endif; ?></figcaption>
 	</figure>
   <?php endif; ?>
