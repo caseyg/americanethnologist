c6ddbde086229c5e475e882bf2911b5325386d6d
Casey Gollan <casey@bullshit.systems>
2017-12-07 14:59:13 -0500

    switch from gtag to analytics.js

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -23,15 +23,21 @@
       $caption.text( flkty.selectedElement.alt )
     });
   </script>
-  <script async data-no-instant src="https://www.googletagmanager.com/gtag/js?id=UA-48247744-1"></script>
+  <!-- Google Analytics -->
+  <script data-no-instant>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48247744-1', 'auto');
+  ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->
   <?php if(!$site->user()): ?>
     <script data-no-instant>
       InstantClick.on('change', function() {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-48247744-1');
+        ga('send', 'pageview', location.pathname + location.search);
       });
 
       InstantClick.init('mousedown');
