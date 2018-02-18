5d75447ca260a437a716c4de439c5f40b9976d41
Casey Gollan <casey@bullshit.systems>
2018-02-17 19:43:08 -0500

    fixes #32

--- site/patterns/profile-cards/profile-cards.html.php
+++ site/patterns/profile-cards/profile-cards.html.php
@@ -1,17 +1,25 @@
 <div class="row">
   <?php foreach ($page->children()->visible() as $p): ?>
-    <div class="col-xs-4">
-      <div class="card">
-        <img class="card-img-top img-fluid" src="<?php if($p->hasImages()): echo $p->images()->first()->crop(300,300)->url(); else: echo 'http://placehold.it/300x300'; endif ?>" alt="Card image cap">
-        <div class="card-block">
-          <?php if ($p->title()->isNotEmpty()): ?><h4 class="card-title"><?php echo $p->title() ?></h4><?php endif; ?>
-          <?php if ($p->position()->isNotEmpty()): ?><p class="sans"><strong><?php echo $p->position() ?></strong><br> <?php echo $p->term() ?></p><?php endif; ?>
-          <?php if ($p->affiliation()->isNotEmpty()): ?><p class="sans"><?php echo $p->affiliation() ?></p><?php endif; ?>
-          <?php if ($p->homepage()->isNotEmpty()): ?><p class="person-meta sans"><a href="<?php echo $p->homepage() ?>"><i class="fa fa-external-link" aria-hidden="true"></i> <?php echo parse_url($p->homepage(),PHP_URL_HOST); ?></a><br><?php endif; ?>
-          <?php if ($p->phone()->isNotEmpty()): ?><i class="fa fa-phone" aria-hidden="true"></i> <?php echo $p->phone() ?></p><?php endif; ?>
-          <?php if ($p->email()->isNotEmpty()): ?><a href="mailto:<?php echo $p->email() ?>"><i class="fa fa-envelope" aria-hidden="true"></i> <?php echo $p->email() ?></a></p><?php endif; ?>
-        </div>
-      </div>
+    <div class="col-xs-6 col-lg-4 mb-2">
+      <img class="img-fluid mb-1" src="<?php if($p->hasImages()): echo $p->images()->first()->crop(300,300)->url(); else: echo 'http://placehold.it/300x300'; endif ?>">
+      <?php if ($p->title()->isNotEmpty()): ?><h4 class="card-title"><?php echo $p->title() ?></h4><?php endif; ?>
+      <?php if ($p->position()->isNotEmpty()): ?><p class="sans"><strong><?php echo $p->position() ?></strong><br> <?php echo $p->term() ?></p><?php endif; ?>
+      <?php if ($p->affiliation()->isNotEmpty()): ?><p class="sans"><?php echo $p->affiliation() ?></p><?php endif; ?>
+      <?php if ($p->homepage()->isNotEmpty()): ?><p class="person-meta sans"><a href="<?php echo $p->homepage() ?>"><i class="fa fa-external-link" aria-hidden="true"></i> Website</a><br><?php endif; ?>
+      <?php if ($p->phone()->isNotEmpty()): ?><i class="fa fa-phone" aria-hidden="true"></i> <?php echo $p->phone() ?></p><?php endif; ?>
+      <?php if ($p->email()->isNotEmpty()): ?><a href="mailto:<?php echo $p->email() ?>"><i class="fa fa-envelope" aria-hidden="true"></i> Email</a></p><?php endif; ?>
     </div>
   <?php endforeach; ?>
 </div>
+<style type="text/css">
+@media (min-width:1201px) {
+  .row>.col-lg-4:nth-child(3n+1) {
+    clear: left;
+  }
+}
+@media (max-width: 1200px) {
+  .row>.col-xs-6:nth-child(odd) {
+    clear: left;
+  }
+}
+</style>
