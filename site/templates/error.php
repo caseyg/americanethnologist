368cfbb5ee37ce724b28fad733921caf396a45d6
Casey Gollan <casey@bullshit.systems>
2018-05-10 17:35:50 -0400

    improve 404, fixes #69

--- site/templates/error.php
+++ site/templates/error.php
@@ -2,29 +2,6 @@
 
 <?php pattern('heading-title') ?>
 
-<?php if ($page->children()->filterBy('slug', '!=', 'modules')->count() > 0 && $page->depth() == 1 || $page->depth() > 1 ): ?>
-
-  <div class="row">
-    <div class="col-sm-3 push-sm-9">
-      <?php if ($page->depth() > 1): ?>
-        <?php pattern('navigation/sidebar', array('pages' => $page->parent()->children()->visible())) ?>
-      <?php else: ?>
-        <?php pattern('navigation/sidebar', array('pages' => $page->children()->visible())) ?>
-      <?php endif ?>
-    </div>
-    <div class="col-sm-8 pull-sm-3">
-      <?php $page->modules() ?>
-    </div>
-  </div>
-
-<?php else: ?>
-
-  <div class="row">
-    <div class="col-xs-12">
-      <?php $page->modules() ?>
-    </div>
-  </div>
-
-<?php endif; ?>
+<p class="lead">The page you requested cannot be found. Try <a href="#" onClick="history.go(-1)">going back</a>, <a href="/search">searching</a> the site, or getting in <a href="/contact">contact</a>.</p>
 
 <?php snippet('footer') ?>
