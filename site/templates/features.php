d2a65d0e2a2eb1df6d3440e9a2803fa320d2577d
Casey Gollan <casey@bullshit.systems>
2018-05-11 11:36:08 -0400

    enlarge images and add rows to features page

--- site/templates/features.php
+++ site/templates/features.php
@@ -6,35 +6,33 @@
 <?php endif; ?>
 
 <?php if ($page->depth() == 1): ?>
-  <div class="row">
-      <?php foreach ($page->children()->visible() as $i): ?>
-        <?php if ($i->title() != "Archive"): ?>
-          <div class="col-md-6 mb-1">
-            <h2 class="h4 text-xs-center"><a href="<?php echo $i->url() ?>"><?php echo $i->title() ?></a></h2>
-            <ul class="list-unstyled">
-              <?php $n = 0; ?>
-              <?php foreach ($i->children()->visible()->flip()->limit(3) as $i): $n++; ?>
-                <li class="mb-1">
-                  <a href="<?php echo $i->url() ?>">
-                    <div class="media">
-                      <?php if ($n == 1): ?>
-                        <?php if ($i->featuredImage()): ?>
-                          <img class="media-object img-fluid mb-1" src="<?php echo $i->featuredImage()->toFile()->crop(400,250)->url() ?>">
-                        <?php endif; ?>
-                      <?php endif; ?>
-                      <div class="media-body">
-                        <h5 class="media-heading bentonsansrebold" style="line-height:1.35;"><?php echo $i->title() ?></h6>
-                        <h6 class="bentonsansreregular"><?php echo $i->date("M d, Y") ?></h6>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              <?php endforeach; ?>
-            </ul>
-          </div>
-        <?php endif; ?>
-      <?php endforeach; ?>
-  </div>
+  <?php $r=0; foreach ($page->children()->visible() as $i): ?>
+  	<?php if($r%2 === 0) echo '<div class="row">' ?>
+      <div class="col-md-6 mb-1">
+        <h2 class="h4 text-xs-center"><a href="<?php echo $i->url() ?>"><?php echo $i->title() ?></a></h2>
+        <ul class="list-unstyled">
+          <?php $n = 0; ?>
+          <?php foreach ($i->children()->visible()->flip()->limit(3) as $i): $n++; ?>
+            <li class="mb-1">
+              <a href="<?php echo $i->url() ?>">
+                <div class="media">
+                  <?php if ($n == 1): ?>
+                    <?php if ($i->featuredImage()): ?>
+                      <img class="media-object img-fluid mb-1" src="<?php echo $i->featuredImage()->toFile()->crop(540,338)->url() ?>">
+                    <?php endif; ?>
+                  <?php endif; ?>
+                  <div class="media-body">
+                    <h5 class="media-heading bentonsansrebold" style="line-height:1.35;"><?php echo $i->title() ?></h6>
+                    <h6 class="bentonsansreregular"><?php echo $i->date("M d, Y") ?></h6>
+                  </div>
+                </div>
+              </a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+  	<?php if($r%2 === 1) echo '</div>' ?>
+  <?php $r++; endforeach; ?>
 
   <hr>
 <?php endif; ?>
